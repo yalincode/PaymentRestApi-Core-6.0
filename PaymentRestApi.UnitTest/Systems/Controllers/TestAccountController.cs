30ca0daec02b4af35f71eb4f2a05273c4c033727
yalincode <yukselyal@gmail.com>
2022-05-18 11:11:27 +0300

    Unit test created

---
+++ PaymentRestApi.UnitTest/Systems/Controllers/TestAccountController.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PaymentRestApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentRestApi.UnitTest.System.Controllers;
+
+public class TestAccountController
+{
+    [Fact]
+    public void Get_OnSuccess_ReturnsStatusCode200()
+    {
+        //Arrange
+        var sut = new AccountController();
+
+        //Act=>Account info unit test return 200 status code for AccountNumber 100 
+        var result = (OkObjectResult)sut.AccountInfo(100);
+
+
+        //Assert
+        result.StatusCode.Should().Be(200);
+    }
+}
---
+++ PaymentRestApi.UnitTest/Systems/Controllers/TestTransactionController.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PaymentRestApi.Controllers;
+using PaymentRestApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentRestApi.UnitTest.Systems.Controllers
+{
+    public class TestTransactionController
+    {
+        [Fact]
+        public void Get_OnSuccess_ReturnsStatusCode200()
+        {
+            //Arrange
+            var sut = new TransactionController();
+
+            //Act=>Account info unit test return 200 status code for AccountNumber 100 
+            var deposit = new DepositDTO()
+            {
+                accountNumber = 100,
+                amount =50,
+                
+            };
+            var result = (OkObjectResult)sut.Deposit(deposit);
+
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
